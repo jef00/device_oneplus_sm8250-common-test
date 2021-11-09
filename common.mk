81020e7ae8f66949a85d0fb508e929ce7e00b844
TH779 <i@779.moe>
2021-11-09 15:28:11 +0800

    sm8250-common: Uprev fingerprint HAL to 2.3
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: Ia36a9f6f2ed866824a95c2763eae4da15293c448

--- common.mk
+++ common.mk
@@ -283,8 +283,10 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.oneplus \
-    vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
+    android.hardware.biometrics.fingerprint@2.3-service.oneplus_kona \
+    android.hardware.biometrics.fingerprint@2.3.vendor \
+    vendor.oneplus.fingerprint.extension@1.0 \
+    vendor.oneplus.hardware.display@1.0
 
 # GPS
 PRODUCT_COPY_FILES += \
